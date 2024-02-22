067c7db67bacfcf6c2ad3c214ab26cba5fabc5b5
Atif CCC <atif.h@ccc.training>
2024-02-22 23:14:09 +0530

    chore: config

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,8 +2,10 @@
 
 import axios from "axios";
 
+import config from "config/config";
+
 const instance = axios.create({
-  baseURL: "http://localhost:3001", // Set your base URL
+  baseURL: `${config.apiBaseUrl}`, // Set your base URL
   withCredentials: true, // Allow credentials globally
 });
 
